c0dab6b73417936acaf4e1eb365d25d208c02c9e
mikeyphondo <mikeyphondo@gmail.com>
2026-02-24 13:20:32 +0100

    Mise à jour de la version en 2.6

--- main.py
+++ main.py
@@ -67,7 +67,7 @@ def maj_menu(role="invite"):
         menu_admin.add_command(label="Paramètres", command=fenetre_reglages)
     
 
-    barre_menu.add_command(label="A propos", command=messagebox.showinfo("Version", "Version actuelle de l'application : 2.5"))
+    barre_menu.add_command(label="A propos", command=messagebox.showinfo("Version", "Version actuelle de l'application : 2.6"))
 
 # ECRAN D'ACCEUIL
 def ecran_accueil():
